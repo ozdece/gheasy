50576151b6ba6ffd21ff6c9f0198358a491bf19e
Cem Özden <ozdenc64@gmail.com>
2025-08-16 21:34:40 +0200

    Adding Spock Framework Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("groovy")
 }
 
 group = "com.ozdece"
@@ -10,10 +11,17 @@ repositories {
 }
 
 dependencies {
+    testImplementation("org.spockframework:spock-core:2.4-M6-groovy-4.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.slf4j:slf4j-api:2.0.17")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("started", "passed", "skipped", "failed")
+    }
 }
